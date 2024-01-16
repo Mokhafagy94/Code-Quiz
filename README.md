8233d13296657580ad80972fedb7e50acc815c32
Mokhafagy94 <mokhafagy94@gmail.com>
2024-01-16 21:58:29 +0000

    upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Code-Quiz
 Challenge 6
+
+## User Story
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers.
+
+
+## Technologies Used
+
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://materializecss.com/)
+
+
+
+## Mock-Up
+
+The following animation demonstrates the application functionality:
+
+![Animation of code quiz. Presses button to start quiz. Clicks the button for the answer to each question, displays if answer was correct or incorrect. Quiz finishes and displays high scores. User adds their intials, then clears their intials and starts over.](./assets/08-web-apis-challenge-demo.gif)
---
+++ questions .js
@@ -0,0 +1,31 @@
+var questions = [
+  {
+    title: "Commonly used data types Do Not Include:",
+    multiChoice: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
+    answer: "2.booleans"
+  },
+
+  {
+    title: "The condition in an if/else statment is enclosed within_____.",
+    multiChoice: ["1.quotes", "2.curly brackets", "3.parentheses", "4.square brackets"],
+    answer: "3.parenthese"
+  },
+
+  {
+    title: "Arrays in JavaScript can be used to store _____.",
+    multiChoice: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+    answer: "4.all of the above"
+  },
+
+  {
+    title: "String values must be enclosed within ____ when being assigned to variables",
+    multiChoice: ["1.commas", "2.curly brackets", "3.quotes", "4.parentheses"],
+    answer: "2.curly brackets"
+  },
+
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is :",
+    multiChoice: ["1.JavaScript", "2.terminal/bath", "for loops", "console log"],
+    answer: "1.JavaScript"
+  }
+];
